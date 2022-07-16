0f0dfcdaf2841b07841dc50eb6b62ebd3629f861
lcb931023 <lcb931023@gmail.com>
2022-07-16 21:11:33 +0800

    add hover effect for event log action titles

--- src/components/ActionCard.vue
+++ src/components/ActionCard.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="mx-8 my-4 p-4 relative border" :class="{'line-through': isForecast}">
+<div class="mx-8 my-4 p-4 relative border">
   <div class="dropdown dropdown-end absolute right-0 top-0 bg-primary">
     <span tabindex="0" class="w-4 h-4 flex items-center justify-center">
       <font-awesome-icon icon="ellipsis-vertical" class="text-base" />
@@ -16,16 +16,18 @@
   <span
     class="text-2xl cursor-pointer"
     :class="{
-      'badge badge-success inline': (isCommitted && !isForecast),
+      'hover:badge hover:badge-success hover:inline': (!isCommitted && !isForecast),
+      'badge badge-success inline hover:line-through': (isCommitted && !isForecast),
+      'line-through hover:no-underline': isForecast,
     }"
     @click="toggleCardMode"
     >{{name}}</span>
-  <p>
+  <p :class="{'line-through': isForecast}">
     <span v-if="Number.isFinite(confidence)" class="badge badge-ghost text-lg mr-1">Confidence: {{confidence}}% </span>
     <span v-if="Number.isFinite(risk)" class="badge badge-ghost text-lg mr-1">Risk: d{{risk}} </span>
     <span v-if="Number.isFinite(compute)" class="badge badge-ghost text-lg mr-1">Compute: {{compute}} </span>
   </p>
-  <p class=" mt-4 normal-case text-lg whitespace-pre-wrap" v-if="description">{{description}}</p>
+  <p class=" mt-4 normal-case text-lg whitespace-pre-wrap" :class="{'line-through': isForecast}" v-if="description">{{description}}</p>
 </div>
 </template>
 
