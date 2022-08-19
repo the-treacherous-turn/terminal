f7036491ea3038640f30648b7628d7949839ca08
lcb931023 <lcb931023@gmail.com>
2022-08-20 01:45:45 +0800

    add onboarding screen

---
+++ src/Onboarding.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data() {
+    return {
+      sessionID: ''
+    }
+  },
+  methods: {
+    openSession() {
+      window.location.hash = this.sessionID
+    }
+  }
+}
+</script>
+
+<template>
+<div class="flex flex-col items-center justify-center h-full">
+  <img src="/logo.png" alt="logo" class="w-80">
+  <h1 class="text-6xl pb-4 highlighter">The Treacherous Turn_</h1>
+  <p class="pb-4">To start, enter your session ID:</p>
+  <input v-model="sessionID" type="text" class="input input-sm input-bordered text-center">
+  <button class="btn btn-wide mt-8" :disabled="!sessionID" @click="openSession">Open Game Session</button>
+</div>
+</template>
--- src/main.js
+++ src/main.js
@@ -6,9 +6,19 @@ import { faItchIo, faTwitch, faReddit, faTwitter } from '@fortawesome/free-brand
 library.add(fas, faItchIo, faTwitch, faReddit, faTwitter)
 import store from './store'
 import App from './App.vue'
+import Onboarding from './Onboarding.vue'
 import './index.css'
 
 const app = createApp(App)
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(store)
-app.mount('#app')
+
+// If user arrives at plain url,
+// then load the onboarding view
+const hash = document.location.hash
+if (!hash) {
+  const onboarding = createApp(Onboarding)
+  onboarding.mount('#app')
+} else {
+  app.mount('#app')
+}
