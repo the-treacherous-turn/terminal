496e08a14e6787da091ae5d830b7ac302d20e425
lcb931023 <lcb931023@gmail.com>
2022-09-28 12:16:37 +0800

    create info section fields

--- src/components/InfoSection.vue
+++ src/components/InfoSection.vue
@@ -1,6 +1,49 @@
 <template>
   <div>
-    <h1 class="text-3xl">TODO</h1>
-    <p class="text-xl">Character and campaign info.</p>
+    <div class="form-control w-full max-w-lg">
+      <h1 class="text-3xl pb-2">Campaign Info</h1>
+
+      <label class="label font-semibold">
+        <span class="label-text">AGI name</span>
+      </label>
+      <input type="text" class="input input-bordered w-full max-w-xs" />
+
+      <label class="label font-semibold">
+        <span class="label-text">Intended Function & Terminal Goals</span>
+      </label>
+      <textarea class="textarea textarea-bordered"></textarea>
+
+      <label class="label font-semibold">
+        <span class="label-text">AGI Details & Safety Measures</span>
+      </label>
+      <textarea class="textarea textarea-bordered"></textarea>
+
+      <label class="label font-semibold">
+        <span class="label-text">Scenario Notes</span>
+      </label>
+      <textarea class="textarea textarea-bordered" placeholder="For difficulty, compute scarcity, differences from modern day, etc"></textarea>
+    </div>
+
+    <div class="form-control w-full max-w-lg">
+      <h1 class="text-3xl pb-2">In-Play Info</h1>
+
+      <label class="label font-semibold">
+        <span class="label-text">Instrumental Goals</span>
+      </label>
+      <textarea class="textarea textarea-bordered"></textarea>
+      
+
+      <label class="label font-semibold">
+        <span class="label-text">Assets</span>
+      </label>
+      <textarea class="textarea textarea-bordered"></textarea>
+
+      <label class="label font-semibold">
+        <span class="label-text">Gathered Information</span>
+      </label>
+      <textarea class="textarea textarea-bordered"></textarea>
+
+    </div>
+
   </div>
 </template>
