db4dfb6271ace21ad80184528a7cc932c25e3fa7
lcb931023 <lcb931023@gmail.com>
2022-10-10 12:56:32 +0800

    allow spec manager to scroll

--- src/components/UpgradeSection/OneSpecManager.vue
+++ src/components/UpgradeSection/OneSpecManager.vue
@@ -22,7 +22,7 @@ export default {
 </script>
 
 <template>
-  <div>
+  <div class="relative h-4/5 pb-1/10">
     <button class="btn btn-xs my-4" @click="exitSpec">&lt; Select Spec</button>
     <h1 class="text-3xl font-bold pb-2">{{activeSpec.name}}</h1>
     <Upgrades />
