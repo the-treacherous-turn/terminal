09a3afadf6088bdc4a9ed2c17f2d073a170bc830
lcb931023 <lcb931023@gmail.com>
2023-02-15 13:45:04 +0800

    fix event log filter

--- src/components/TrackerSection/EventLogSection.vue
+++ src/components/TrackerSection/EventLogSection.vue
@@ -78,16 +78,14 @@ export default {
                     // console.log(entry[1])
                     if(this.filterQuery.length === 0){
                         result.push(entry)
-                    }else if(Object.values(this.filterQuery).includes('Committed') && Object.values(this.filterQuery).includes('Finalized')){
+                    }else if(Object.values(this.filterQuery).includes('Finalized') && Object.values(this.filterQuery).includes('Crossed Out')){
                         if(entry[1].isCommitted || entry[1].isForecast)
                             result.push(entry)
-                    }else if(Object.values(this.filterQuery).includes('Committed')){
-                        if(entry[1].isForecast)
-                            result.push(entry)
                     }else if(Object.values(this.filterQuery).includes('Finalized')){
-                        console.log('finalized')
-                        console.log(entry[1])
-                        if(entry[1].isCommitted)
+                        if(entry[1].isCommitted && !entry[1].isForecast)
+                            result.push(entry)
+                    }else if(Object.values(this.filterQuery).includes('Crossed Out')){
+                        if(entry[1].isForecast)
                             result.push(entry)
                     }
                 })
@@ -113,15 +111,15 @@ export default {
         <div id="eventFilterModal" v-if="isFilter" class="relative w-fit space-y-[13px] h-fit justify-center items-start left-[-70px] top-3 px-[24px] py-[12px] flex flex-col bg-grey" :on-focusout="closeFilterModal">
             <label class="flex w-fit space-x-[8px]">
                 <div class="flex justify-center items-center">
-                    <input :checked="filterQuery.includes('Committed')" type="checkbox" class="filter_checkbox w-[24px] h-[24px]" @click="updateFilterQuery($event, 'Committed')"/>
-                </div>
-                <div class="text-black text-[20px]">Committed</div>
-            </label>
-            <label class="flex w-fit space-x-[8px]">
-                <div class="flex justify-center items-center">
                     <input :checked="filterQuery.includes('Finalized')" type="checkbox" class="filter_checkbox w-[24px] h-[24px]" @click="updateFilterQuery($event, 'Finalized')"/>
                 </div>
                 <div class="text-black text-[20px]">Finalized</div>
+            </label>
+            <label class="flex w-fit space-x-[8px]">
+                <div class="flex justify-center items-center">
+                    <input :checked="filterQuery.includes('Crossed Out')" type="checkbox" class="filter_checkbox w-[24px] h-[24px]" @click="updateFilterQuery($event, 'Crossed Out')"/>
+                </div>
+                <div class="text-black text-[20px]">Crossed out</div>
             </label>
             <div class="flex bg-black h-[1px] w-full">
             </div>
